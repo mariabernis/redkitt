42c5c7c80fe723e80ac0751d3c9f8cb9d9d6832f
Pol Quintana <polkintana@gmail.com>
2014-12-13 15:00:12 +0100

    Add Task VC preparation

---
+++ RedKitt WatchKit Extension/AddTaskInterfaceController.m
@@ -0,0 +1,42 @@
+//
+//  AddTaskInterfaceController.m
+//  RedKitt
+//
+//  Created by Pol Quintana on 13/12/14.
+//  Copyright (c) 2014 Pol Quintana. All rights reserved.
+//
+
+#import "AddTaskInterfaceController.h"
+
+
+@interface AddTaskInterfaceController()
+
+@end
+
+
+@implementation AddTaskInterfaceController
+
+- (instancetype)initWithContext:(id)context {
+    self = [super initWithContext:context];
+    if (self){
+        // Initialize variables here.
+        // Configure interface objects here.
+        NSLog(@"%@ initWithContext", self);
+    }
+    return self;
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    NSLog(@"%@ will activate", self);
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    NSLog(@"%@ did deactivate", self);
+}
+
+@end
+
+
+
