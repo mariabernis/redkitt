bf50e5aa13a42edeb6a86f8fe97c41abab5e964a
Pol Quintana <polkintana@gmail.com>
2014-12-13 12:35:12 +0100

    Initial Commit

---
+++ RedKitt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RedKitt
+//
+//  Created by Pol Quintana on 13/12/14.
+//  Copyright (c) 2014 Pol Quintana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RedKitt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RedKitt
+//
+//  Created by Pol Quintana on 13/12/14.
+//  Copyright (c) 2014 Pol Quintana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
